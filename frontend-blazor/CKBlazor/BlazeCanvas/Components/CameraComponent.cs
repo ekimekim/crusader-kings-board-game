78a5cc3823553e2833d51a4b22107f07e86a2c72
Don <sleepyparadox@gmail.com>
2025-01-11 15:17:03 +1100

    Fixed camera clear color

--- frontend-blazor/CKBlazor/BlazeCanvas/Components/CameraComponent.cs
+++ frontend-blazor/CKBlazor/BlazeCanvas/Components/CameraComponent.cs
@@ -5,7 +5,7 @@
         public override string JsClassName() => "camera";
         public static object GetArgs(float clearColorR = 0.1f, float clearColorG = 0.1f, float clearColorB = 0.1f) => new
         {
-            clearColor = PCTypes.NewColor(clearColorR, 0.1f, 0.1f),
+            clearColor = PCTypes.NewColor(clearColorR, clearColorG, clearColorB),
         };
     }
 }
