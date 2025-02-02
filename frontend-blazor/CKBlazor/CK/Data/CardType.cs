1845dbba7f3af2ea42e868a6dcd806d60bf7e128
Don <sleepyparadox@gmail.com>
2025-02-02 11:43:20 +1100

    Rough HUD

---
+++ frontend-blazor/CKBlazor/CK/Data/CardType.cs
@@ -0,0 +1,11 @@
+﻿namespace CKBlazor.CK.Data
+{
+    public static class CardType
+    {
+        public const string BuildDevelop = "BUILD | DEVELOP";
+        public const string Plot = "PLOT";
+        public const string Tax = "TAX";
+        public const string MobilizeInvade = "MOBILIZE | INVADE";
+        public const string Crusade = "CRUSADE";
+    }
+}
